60c7e7002444a2a2d73d46ae6980a8eb3323a197
ChaimEzra <133007790+ChaimEzra@users.noreply.github.com>
2024-05-24 16:42:25 +0300

    Add files via upload

---
+++ first.js
@@ -0,0 +1,19 @@
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is: " + d + "</h1>"
+let var1 = 3;
+
+console.log(var1);
+
+let calculation = 2+2;
+
+console.log(calculation + 2);
+
+let message = "Hello";
+console.log(message);
+var1 = 5;
+console.log(var1);
+var1++;
+console.log(var1);
+const var2 = 3;
+var var3 = 4;
+console.log(typeof var3)
